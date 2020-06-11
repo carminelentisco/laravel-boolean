8001052b479250d17fc1398a61a23b93aa99fb6f
carminelentisco <carminelentisco@gmail.com>
2020-06-11 21:14:36 +0200

    Footer { Modifiche alla struttura base }

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,12 +1,28 @@
 
     <footer class="footer">
-        <div class="footer-content">
-            <h2><a href="{{ Route('static-page.home') }}">BOOLEAN</a></h2>
+        <div class="container">
+            
+            <div class="footer-sx">
+                <h2>
+                    <a href="{{ Route('static-page.home') }}"><img src="https://www.boolean.careers/images/common/logo-white.png" alt="logo boolean bianco"></a>
+                </h2>
 
-        <ul class="list-style">
-            <li><a href="{{ Route('static-page.privacy') }}">Privacy</a></li>
-            <li><a href="{{ Route('static-page.faq') }}">Faq</a></li>
-        </ul>
+                <ul class="footer-list-info list-style">
+                    <li>Sede operativa</li>
+                    <li>Via Carducci 12 - 20123 Milano</li>
+                    <li>Tel: 02-40031288</li>
+                </ul>
+            </div>
+
+            <div class="footer-dx">
+                <ul class="footer-action list-style">
+                    <li><a href="#">Lezione gratuita</a></li>
+                    <li><a href="{{ Route('static-page.privacy') }}">Privacy</a></li>
+                    <li><a href="{{ Route('static-page.faq') }}">Faq</a></li>
+                    <li><a href="#">Lavora con noi</a></li>
+                </ul>
+            </div>
+            
         </div>
     </footer>
 
