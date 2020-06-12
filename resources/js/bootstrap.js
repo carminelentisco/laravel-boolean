b8c4ce4f07bc3768fc549bdaaddf5f1d82f54f88
carminelentisco <carminelentisco@gmail.com>
2020-06-12 16:51:39 +0200

    bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,6 @@
-window._ = require('lodash');
+window.$ = require('jquery');
+window.Handlebars = require('handlebars');
+
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
