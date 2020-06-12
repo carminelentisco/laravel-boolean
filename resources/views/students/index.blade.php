95106816a5a9cf109da71ee0288a85e53d1de503
carminelentisco <carminelentisco@gmail.com>
2020-06-12 16:52:02 +0200

    index

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,9 +3,20 @@
 @section('main-content')
     <h1 class="title-center student-title">I nostri ex studenti su LinkedIn</h1>
 
+    {{-- Aggiunta sezione per il filter dei generi --}}
+    <div class="students-filter">
+        <select name="filter" id="filter">
+            @foreach ($genders as $gender) 
+                <option value="{{ $gender }}">
+                    @if ($gender == 'm') Uomo @elseif ($gender == 'f') Donna @else Tutti @endif
+                </option>
+            @endforeach
+        </select>
+    </div>
+
     <div class="students">
         @foreach($students as $student)
-        <a class="student" href="{{ route('students-page.show', ['id' => $student['id'] ]) }}">
+        <a class="student" href="{{ route('students-page.show', ['slug' => $student['slug'] ]) }}">
             <header>
                 <img src="{{$student['img']}}" alt="{{$student['nome']}} {{$student['cognome']}}">
                 
@@ -21,4 +32,11 @@
         </a>
         @endforeach
     </div>
+
+    @include('shared.handlebars.student')
+
+@endsection
+
+@section('script')
+    <script src="{{ asset('js/app.js') }}"></script>
 @endsection
