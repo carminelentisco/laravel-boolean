5e15c08d0116266190858f67b5b2a1dd47c749d0
carminelentisco <carminelentisco@gmail.com>
2020-06-12 09:44:07 +0200

    Studenti Home { Modifiche alla struttura }

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,14 +1,18 @@
 @extends('layout.main')
 
 @section('main-content')
+    <h1 class="title-center student-title">I nostri ex studenti su LinkedIn</h1>
 
     <div class="students">
         @foreach($students as $student)
         <a class="student" href="{{ route('students-page.show', ['id' => $student['id'] ]) }}">
             <header>
-                <img src="{{$student['img']}}" alt="">
-                <h2>{{$student['nome']}} {{$student['cognome']}} ( {{$student['eta']}} anni)</h2>
-                <p>Assunt{{ ($student['genere'] == 'm') ? 'o' : 'a' }} da {{$student['azienda']}} come {{$student['ruolo']}}</p>
+                <img src="{{$student['img']}}" alt="{{$student['nome']}} {{$student['cognome']}}">
+                
+                <div class="header-student-text">
+                    <h2>{{$student['nome']}} {{$student['cognome']}} ( {{$student['eta']}} anni)</h2>
+                    <p>Assunt{{ ($student['genere'] == 'm') ? 'o' : 'a' }} da {{$student['azienda']}} come {{$student['ruolo']}}</p>
+                </div>
             </header>
 
             <main>
